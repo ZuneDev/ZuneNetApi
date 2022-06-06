bb17649517dd55c7aae6665a5160c1de058a86b7
Yoshi Askharoun <jjask7@gmail.com>
2022-06-06 15:41:32 -0500

    Added more default rights

--- Zune.Net.Shared/Helpers/MusicBrainz.cs
+++ Zune.Net.Shared/Helpers/MusicBrainz.cs
@@ -23,7 +23,57 @@ namespace Zune.Net.Shared.Helpers
                 OfferId = Guid.Parse("9534a201-2102-11db-89ca-0019b92a3933"),
                 FileSize = 500
             });
+
+            if (media is Album album)
+                AddDefaultAlbumRights(ref album);
+            else if (media is Track track)
+                AddDefaultTrackRights(ref track);
+
             media.Actionable = true;
+        }
+
+        public static void AddDefaultAlbumRights(ref Album album)
+        {
+            album.Rights ??= new();
+            album.Rights.Add(new()
+            {
+                MediaInstanceId = "3b871100-1000-11db-89ca-0019b92a3936",
+                ProviderCode = "117767492:MP3_DOWNLOAD_UENC_256kb_075",
+                Price = 0,
+                CurrencyCode = PriceTypeEnum.Points,
+                LicenseType = "Download",
+                LicenseRight = MediaRightsEnum.AlbumPurchase,
+                AudioEncoding = AudioEncodingEnum.MP3,
+                OfferId = Guid.Parse("9534a201-2102-11db-89ca-0019b92a3936"),
+            });
+            album.Actionable = true;
+        }
+
+        public static void AddDefaultTrackRights(ref Track track)
+        {
+            track.Rights ??= new();
+            track.Rights.Add(new()
+            {
+                MediaInstanceId = "3b871100-1000-11db-89ca-0019b92a3934",
+                ProviderCode = "117767492:MP3_DOWNLOAD_UENC_256kb_075",
+                Price = 0,
+                CurrencyCode = PriceTypeEnum.Points,
+                LicenseType = "MP3 Purchase",
+                LicenseRight = MediaRightsEnum.Purchase,
+                AudioEncoding = AudioEncodingEnum.MP3,
+                OfferId = Guid.Parse("9534a201-2102-11db-89ca-0019b92a3934"),
+            });
+            track.Rights.Add(new()
+            {
+                MediaInstanceId = "3b871100-1000-11db-89ca-0019b92a3935",
+                ProviderCode = "117767492:MP3_DOWNLOAD_UENC_256kb_075",
+                Price = 0,
+                CurrencyCode = PriceTypeEnum.Points,
+                LicenseType = "WMA Preview",
+                LicenseRight = MediaRightsEnum.Preview,
+                AudioEncoding = AudioEncodingEnum.WMA,
+                OfferId = Guid.Parse("9534a201-2102-11db-89ca-0019b92a3935"),
+            });
         }
     }
 }
