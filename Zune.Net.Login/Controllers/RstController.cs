7dce72fb12ab796a69decc838ee4be417920b272
Yoshi Askharoun <jjask7@gmail.com>
2022-06-22 15:15:41 -0500

    Pulled data from sign in response

--- Zune.Net.Login/Controllers/RstController.cs
+++ Zune.Net.Login/Controllers/RstController.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
 using Zune.DB;
 
 namespace Zune.Net.Login.Controllers
@@ -9,6 +14,7 @@ namespace Zune.Net.Login.Controllers
     public class RstController : ControllerBase
     {
         const string LOGIN_LIVE_COM = "login.live.com";
+        const string CREDPROP_XPATH = "//psf:credProperty[@Name=\"{0}\"]";
 
         private readonly ZuneNetContext _database;
         public RstController(ZuneNetContext database)
@@ -38,9 +44,28 @@ namespace Zune.Net.Login.Controllers
             Response.ContentLength = response.Content.Headers.ContentLength;
             Response.ContentType = Atom.Constants.SOAP_XML_MIMETYPE + "; charset=utf-8";
 
+            // Save relevant information to database
+            string xml = await response.Content.ReadAsStringAsync();
+            var doc = new XPathDocument(new StringReader(xml));
+            XPathNavigator nav = doc.CreateNavigator();
+            XmlNamespaceManager namespaceResolver = new(nav.NameTable);
+            namespaceResolver.AddNamespace("psf", "http://schemas.microsoft.com/Passport/SoapServices/SOAPFault");
+
+            var cidElem = nav.SelectSingleNode(string.Format(CREDPROP_XPATH, "CID"), namespaceResolver);
+            var emailElem = nav.SelectSingleNode(string.Format(CREDPROP_XPATH, "AuthMembername"), namespaceResolver);
+            var countryElem = nav.SelectSingleNode(string.Format(CREDPROP_XPATH, "Country"), namespaceResolver);
+            var languageElem = nav.SelectSingleNode(string.Format(CREDPROP_XPATH, "Language"), namespaceResolver);
+            var firstNameElem = nav.SelectSingleNode(string.Format(CREDPROP_XPATH, "FirstName"), namespaceResolver);
+            var lastNameElem = nav.SelectSingleNode(string.Format(CREDPROP_XPATH, "LastName"), namespaceResolver);
+            string? cid = cidElem?.Value;
+            string? email = emailElem?.Value;
+            string? country = countryElem?.Value;
+            int? language = languageElem?.ValueAsInt;
+            string? firstName = firstNameElem?.Value;
+            string? lastName = lastNameElem?.Value;
+
             // Copy body into response
-            await response.Content.ReadAsStream().CopyToAsync(Response.Body);
-            
+            await response.Content.CopyToAsync(Response.Body);
             await Response.CompleteAsync();
         }
     }
--- Zune.Net.Login/Program.cs
+++ Zune.Net.Login/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Zune.Net.Middleware;
 
 namespace Zune.Net.Login;
