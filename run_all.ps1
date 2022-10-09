083b932520b1c753f6d9c3b8e09c18a682763c7a
Yoshi Askharoun <jjask7@gmail.com>
2022-10-08 21:01:10 -0500

    Moved project info type into variable

--- run_all.ps1
+++ run_all.ps1
@@ -1,18 +1,21 @@
 param([bool] $Debug = $true)
 
+$projInfoType = "tuple[String,String]"
+
 $projInfos = @(
-    New-Object "tuple[String,String]" "Zune.Net.MetaServices", "1"
-    New-Object "tuple[String,String]" "Zune.Net.Catalog", "2"
-    # New-Object "tuple[String,String]" "Zune.Net.Commerce", "3"
-    # New-Object "tuple[String,String]" "Zune.Net.ImageCatalog", "4"
-    # New-Object "tuple[String,String]" "Zune.Net.SocialApi", "5"
-    # New-Object "tuple[String,String]" "Zune.Net.Comments", "6"
-    # New-Object "tuple[String,String]" "Zune.Net.Inbox", "7"
-    New-Object "tuple[String,String]" "Zune.Net.Mix", "8"
-    # New-Object "tuple[String,String]" "Zune.Net.Stats", "9"
-    # New-Object "tuple[String,String]" "Zune.Net.Cache", "10"
-    # New-Object "tuple[String,String]" "Zune.Net.Tuners", "11"
-    # New-Object "tuple[String,String]" "Zune.Net.Tiles", "12"
+    # New-Object $projInfoType "Zune.Net.MetaServices", "1"
+    New-Object $projInfoType "Zune.Net.Catalog", "2"
+    New-Object $projInfoType "Zune.Net.Commerce", "3"
+    New-Object $projInfoType "Zune.Net.Catalog.Image", "4"
+    New-Object $projInfoType "Zune.Net.SocialApi", "5"
+    # New-Object $projInfoType "Zune.Net.Comments", "6"
+    # New-Object $projInfoType "Zune.Net.Inbox", "7"
+    New-Object $projInfoType "Zune.Net.Mix", "8"
+    # New-Object $projInfoType "Zune.Net.Stats", "9"
+    # New-Object $projInfoType "Zune.Net.Cache", "10"
+    # New-Object $projInfoType "Zune.Net.Tuners", "11"
+    New-Object $projInfoType "Zune.Net.Tiles", "12"
+    New-Object $projInfoType "Zune.Net.Login", "14"
 )
 
 if ($Debug -eq $true)
@@ -31,7 +34,7 @@ foreach ($projInfo in $projInfos)
     
     $exePath = ".\" + $projName + "\bin\" + $configuration + "\net6.0\" + $projName + ".exe"
 
-    $command = '$env:ASPNETCORE_URLS=\"http://127.0.0.'+ $hostNum + ':80;https://127.0.0.'+ $hostNum + ':443\"; ' + $exePath
+    $command = '$env:ASPNETCORE_URLS=\"http://127.0.0.' + $hostNum + ':80;https://127.0.0.' + $hostNum + ':443\"; echo "Starting ' + $projName + '..."; ' + $exePath
     echo $command
     
     Start-Process powershell.exe -ArgumentList "-noexit", ("-command " + $command)
