289274d23c8934450c68937009b4b576561e114f
Yoshi Askharoun <jjask7@gmail.com>
2022-12-20 12:31:47 -0600

    Login & sign up experiments

---
+++ Zune.Net.Login/Controllers/ConfigController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Zune.Net.Login.Controllers
+{
+    [Route($"/{PPCRLCONFIG_NAME}")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        internal const string PPCRLCONFIG_NAME = "ppcrlconfig.bin";
+
+        private readonly IWebHostEnvironment _env;
+
+        public ConfigController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string path = Path.Combine(_env.ContentRootPath, "Resources", PPCRLCONFIG_NAME);
+            return PhysicalFile(path, "application/octet-stream", PPCRLCONFIG_NAME, true);
+        }
+    }
+}
---
+++ Zune.Net.SocialApi/Controllers/Html2TextController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Zune.Net.SocialApi.Controllers
+{
+    [Route("/html2text.ashx")]
+    [ApiController]
+    public class Html2TextController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public Html2TextController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [Route("touFragments/LiveTOU_{locale}.htm")]
+        public ActionResult<string> LiveTOU(string locale)
+        {
+            string path = Path.Combine(_env.ContentRootPath, "Resources", "LiveTOU.html");
+            string html = System.IO.File.ReadAllText(path);
+
+            return Content(new HtmlToText().Convert(html), "text/plain");
+        }
+    }
+}
