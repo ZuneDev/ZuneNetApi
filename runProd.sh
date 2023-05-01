3314d8adbaa6b1b0eb750c6b1a5c0978aeaea89e
Haley Grandle <xerootg@users.noreply.github.com>
2023-04-30 23:43:07 -0600

    make runProd do important things like recreate the containers. Maybe someday, ill add the restart policy

--- runProd.sh
+++ runProd.sh
@@ -2,5 +2,5 @@
 docker-compose stop
 docker-compose build
 # if the images aren't getting refreshed, make sure to run docker-compose up first
-docker-compose up --no-start --prune-orphans --restart unless-stopped
+docker-compose up -d #--no-start --prune-orphans --restart unless-stopped
 docker-compose start
