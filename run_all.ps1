22991212410d1e1c84abf9facebae6eb1ed44e88
Yoshi Askharoun <jjask7@gmail.com>
2023-10-08 20:43:12 -0500

    Allow services to be exluded from run_all script

--- run_all.ps1
+++ run_all.ps1
@@ -1,21 +1,21 @@
-param([bool] $Debug = $true)
+param([bool] $Debug = $true, [String[]] $ExcludedServices = @())
 
 $projInfoType = "tuple[String,String]"
 
 $projInfos = @(
-    # New-Object $projInfoType "Zune.Net.MetaServices", "1"
-    New-Object $projInfoType "Zune.Net.Catalog", "2"
-    New-Object $projInfoType "Zune.Net.Commerce", "3"
-    New-Object $projInfoType "Zune.Net.Catalog.Image", "4"
-    New-Object $projInfoType "Zune.Net.SocialApi", "5"
-    # New-Object $projInfoType "Zune.Net.Comments", "6"
-    # New-Object $projInfoType "Zune.Net.Inbox", "7"
-    New-Object $projInfoType "Zune.Net.Mix", "8"
-    # New-Object $projInfoType "Zune.Net.Stats", "9"
-    # New-Object $projInfoType "Zune.Net.Cache", "10"
-    # New-Object $projInfoType "Zune.Net.Tuners", "11"
-    New-Object $projInfoType "Zune.Net.Tiles", "12"
-    New-Object $projInfoType "Zune.Net.Login", "14"
+    # New-Object $projInfoType "MetaServices", "1"
+    New-Object $projInfoType "Catalog", "2"
+    New-Object $projInfoType "Commerce", "3"
+    New-Object $projInfoType "Catalog.Image", "4"
+    New-Object $projInfoType "SocialApi", "5"
+    # New-Object $projInfoType "Comments", "6"
+    # New-Object $projInfoType "Inbox", "7"
+    New-Object $projInfoType "Mix", "8"
+    # New-Object $projInfoType "Stats", "9"
+    # New-Object $projInfoType "Cache", "10"
+    # New-Object $projInfoType "Tuners", "11"
+    New-Object $projInfoType "Tiles", "12"
+    New-Object $projInfoType "Login", "14"
 )
 
 if ($Debug -eq $true)
@@ -32,7 +32,12 @@ foreach ($projInfo in $projInfos)
     $projName = $projInfo.Item1
     $hostNum = $projInfo.Item2
 
-    $command = "dotnet run --no-build --project " + $projName
+    if ($ExcludedServices -Contains $projName)
+    {
+        continue
+    }
+
+    $command = "dotnet run --no-build --project Zune.Net." + $projName
     echo $command
     
     Start-Process powershell.exe -ArgumentList "-noexit", ("-command " + $command)
