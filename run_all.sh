d347b17eff1b44818a13f46f235e9069200ea78a
Yoshi Askharoun <jjask7@gmail.com>
2024-10-22 23:43:07 -0500

    Remove old bash scripts

--- run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-screen -S Zune.Net.Catalog -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.Catalog && dotnet run --urls="http://0.0.0.0:8001;https://0.0.0.0:8801"'
-screen -S Zune.Net.Commerce -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.Commerce && dotnet run --urls="http://0.0.0.0:8002;https://0.0.0.0:8802"'
-screen -S Zune.Net.Catalog.Image -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.Catalog.Image && dotnet run --urls="http://0.0.0.0:8003;https://0.0.0.0:8803"'
-screen -S Zune.Net.SocialApi -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.SocialApi && dotnet run --urls="http://0.0.0.0:8004;https://0.0.0.0:8804"'
-screen -S Zune.Net.Mix -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.Mix && dotnet run --urls="http://0.0.0.0:8005;https://0.0.0.0:8805"'
-screen -S Zune.Net.Tiles -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.Tiles && dotnet run --urls="http://0.0.0.0:8006;https://0.0.0.0:8806"'
-screen -S Zune.Net.Login -dm bash -c 'cd /opt/ZuneNetApi/Zune.Net.Login && dotnet run --urls="http://0.0.0.0:8007;https://0.0.0.0:8807"'
--- stop_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-screen -X -S Zune.Net.Catalog quit
-screen -X -S Zune.Net.Commerce quit
-screen -X -S Zune.Net.Catalog.Image quit
-screen -X -S Zune.Net.SocialApi quit
-screen -X -S Zune.Net.Mix quit
-screen -X -S Zune.Net.Tiles quit
-screen -X -S Zune.Net.Login quit
