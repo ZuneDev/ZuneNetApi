0d45330ca45fcf0f471b4f460e355dad38087619
Joshua Askharoun <jjask7@gmail.com>
2025-05-30 00:07:20 -0500

    Add sample Docker secrets

---
+++ scripts/dockerSecrets.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export MONGO_INITDB_ROOT_USERNAME=root
+export MONGO_INITDB_ROOT_PASSWORD=ChangeMe
+
+export ZuneNetContext__ConnectionString=mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@mongodb:27017/?authSource=admin
+export ZuneNetContext__DatabaseName=Zune
