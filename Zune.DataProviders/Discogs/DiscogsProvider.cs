5ef063b37553093dfbbea70a94834660aa7db772
Yoshi Askharoun <jjask7@gmail.com>
2025-06-05 23:20:30 -0500

    Use ID mapper in DiscogsProvider

--- Zune.DataProviders/Discogs/DiscogsProvider.cs
+++ Zune.DataProviders/Discogs/DiscogsProvider.cs
@@ -1,6 +1,4 @@
 ﻿using Atom.Xml;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Zune.DataProviders.Discogs;
@@ -9,33 +7,23 @@ public class DiscogsProvider(IMediaIdMapper idMapper) : IArtistBiographyProvider
 {
     public async Task<Content> GetArtistBiography(MediaId id)
     {
-        int? dcid = id.Source switch
+        int? dcid = null;
+
+        if (id.Source.OrdinalEquals(KnownMediaSources.Discogs))
         {
-            KnownMediaSources.Discogs => id.AsInt32(),
-            //KnownMediaSources.MusicBrainz => await GetDcidFromMbid(id.AsGuid()),
-            _ => (await idMapper.MapTo(id, KnownMediaSources.Discogs)).AsInt32(),
-        };
+            dcid = id.AsInt32();
+        }
+        else
+        {
+            var mappedId = await idMapper.MapTo(id, KnownMediaSources.Discogs);
+            dcid = mappedId?.AsInt32();
+        }
 
         if (dcid is null)
             return null;
 
         var dcArtist = await Net.Helpers.Discogs.GetDCArtistByDCID(dcid.Value);
         var dcProfile = dcArtist.Value<string>("profile");
-        return Net.Helpers.Discogs.DCProfileToBiographyContent(dcProfile);
-    }
-
-    private static async Task<int?> GetDcidFromMbid(Guid mbid)
-    {
-        var mbArtist = await Net.Helpers.MusicBrainz.Query.LookupArtistAsync(mbid, MetaBrainz.MusicBrainz.Include.UrlRelationships);
-
-        var dcRelationship = mbArtist.Relationships.FirstOrDefault(rel => rel.Type == "discogs");
-        if (dcRelationship is null)
-            return null;
-
-        Flurl.Url dcWebUrl = dcRelationship.Url.Resource;
-        if (int.TryParse(dcWebUrl.PathSegments.Last(), out int dcid))
-            return dcid;
-
-        return null;
+        return await Net.Helpers.Discogs.DCProfileToBiographyContent(dcProfile);
     }
 }
