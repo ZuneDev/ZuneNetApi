7b9753b7f5b99a8cc14392b3704ea9827bc10920
Yoshi Askharoun <jjask7@gmail.com>
2025-09-07 00:44:41 -0500

    Simplify ICultureFeature.cs

--- Zune.Net.Shared/Features/ICultureFeature.cs
+++ Zune.Net.Shared/Features/ICultureFeature.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿#nullable enable
+
+using System;
+using System.Globalization;
 
 namespace Zune.Net.Features;
 
@@ -6,10 +9,37 @@ public interface ICultureFeature
 {
     string CultureString { get; }
 
-    CultureInfo GetCultureInfo();
+    string? Region { get; }
+    
+    string Language { get; }
 }
 
-public record CultureFeature(string CultureString) : ICultureFeature
+public record CultureFeature : ICultureFeature
 {
-    public CultureInfo GetCultureInfo() => new(CultureString);
+    public CultureFeature(string cultureString)
+    {
+        CultureString = cultureString;
+        
+        var parts = cultureString.Split('-');
+        if (parts.Length < 1)
+            throw new ArgumentException("Expected non-empty culture string");
+        
+        Language = parts[0];
+        if (parts.Length >= 2)
+            Region = parts[1];
+    }
+    
+    public string Language { get; }
+
+    public string? Region { get; }
+    
+    public string CultureString { get; }
+}
+
+public static class ICultureFeatureExtensions
+{
+    public static CultureInfo GetCultureInfo(this ICultureFeature feature)
+    {
+        return new CultureInfo(feature.CultureString);
+    }
 }
