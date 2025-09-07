cd86cb9f1072e4e12378079a369f78ef6ae4df39
Yoshi Askharoun <jjask7@gmail.com>
2025-09-07 03:14:27 -0500

    Revert "Use Apple Music for editorials"
    This reverts commit 0019be11de8be32302fa3dee3b6416c18254ad78.

--- Zune.Net.Catalog/Controllers/Music/FeaturesController.cs
+++ Zune.Net.Catalog/Controllers/Music/FeaturesController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using AppleMusicSharp;
+using System.Xml.Linq;
+using CommunityToolkit.Diagnostics;
 using Zune.DB;
 using Zune.Xml.Catalog;
 
@@ -12,84 +14,103 @@ namespace Zune.Net.Catalog.Controllers.Music
 {
     [Route("/music/features/")]
     [Produces(Atom.Constants.ATOM_MIMETYPE)]
-    public class FeaturesController(ZuneNetContext database, AppleMusicClient amClient) : Controller
+    public class FeaturesController(ZuneNetContext database) : Controller
     {
+        /// <summary>
+        /// The URL of the RSS feed to generate features from.
+        /// <para>
+        /// Currently pulling from NPR's New Music topic.
+        /// </para>
+        /// </summary>
+        private const string RSS_FEED_URL = "https://feeds.npr.org/1108/rss.xml";
+        
+        private static readonly XNamespace PurlContent = "http://purl.org/rss/1.0/modules/content/";
+        
         [HttpGet]
         public async Task<ActionResult<Feed<Feature>>> Features()
         {
-            var grouping = await amClient.GetGroupingAsync("1");
-            var editorials = grouping.Resources.EditorialElements.Values.AsEnumerable();
-            
-            List<Feature> features = [];
-            var lastUpdated = DateTime.MinValue;
+            XElement rssDoc = XElement.Load(RSS_FEED_URL);
+            var rssChannel = rssDoc.Element("channel");
+            Guard.IsNotNull(rssChannel);
 
-            foreach (var editorial in editorials)
+            var rssItems = rssChannel.Elements("item");
+            List<Feature> features = [];
+
+            //Regex rxParagraph = new("<p>(.+?)</p>");
+            Regex rxImg = new("""<img (?<attr>(?<key>[\w-_]+)=['""](?<val>.+?)['""]\s?)+\s*/>""");
+
+            foreach (var rssItem in rssItems)
             {
                 Image image = null;
-                MediaLink link = null;
-
-                var subscriptionCover = editorial.Attributes.EditorialArtwork?.SubscriptionCover;
-                if (subscriptionCover is not null)
+                
+                var encodedContent = rssItem.Element(PurlContent + "encoded")?.Value;
+                if (encodedContent is not null)
                 {
-                    var imageUrl = AppleMusicClient.GetImageUrl(subscriptionCover.Url,
-                        420, 320, resizeMode: ResizeMode.ScaleCrop);
+                    var imgTags = rxImg.Matches(encodedContent);
+                    foreach (Match imgTag in imgTags)
+                    {
+                        if (!imgTag.Success)
+                            continue;
+
+                        var attrKeys = imgTag.Groups["key"].Captures;
+                        var attrValues = imgTag.Groups["val"].Captures;
+                        var attributes = attrKeys
+                            .Zip(attrValues, (k, v) => (k, v))
+                            .ToDictionary(x => x.k.Value, x => x.v.Value);
                         
-                    var imageEntry = await database.AddImageAsync(imageUrl);
+                        // img tags without an alt attribute probably aren't meant to visible
+                        if (!attributes.ContainsKey("alt"))
+                            continue;
+                        
+                        // img tags without a src attribute can't be loaded
+                        if (!attributes.TryGetValue("src", out var imgSrc))
+                            continue;
+                        
+                        // Skip images with invalid URLs
+                        if (!Uri.TryCreate(imgSrc, UriKind.Absolute, out var imgUri))
+                            continue;
+                        
+                        var imageEntry = await database.AddImageAsync(imgSrc);
 
-                    image = new Image
-                    {
-                        Id = imageEntry.Id,
-                        Instances =
-                        [
-                            new ImageInstance
-                            {
-                                Id = imageEntry.Id,
-                                Url = imageEntry.Url
-                            }
-                        ]
-                    };
+                        image = new Image
+                        {
+                            Id = imageEntry.Id,
+                            Instances =
+                            [
+                                new ImageInstance
+                                {
+                                    Id = imageEntry.Id,
+                                    Url = imageEntry.Url,
+                                }
+                            ]
+                        };
+                        break;
+                    }
                 }
-
-                var relationships = editorial.Relationships?.Contents?.Data?.FirstOrDefault();
-                if (relationships is null)
-                    continue;
-
-                if (grouping.Resources.MusicVideos.TryGetValue(relationships.Id, out var video))
-                {
-                    link = new MediaLink
-                    {
-                        Type = relationships.Type,
-                        Target = video.Attributes.Url
-                    };
-                }
-
+                
                 Feature feature = new()
                 {
-                    Id = editorial.Id,
-                    Title = editorial.Attributes.DesignBadge,
-                    Content = editorial.Attributes.DesignTag,
+                    Id = rssItem.Element("guid")?.Value,
+                    Title = rssItem.Element("title")?.Value,
+                    Content = rssItem.Element("description")?.Value,
                     SequenceNumber = features.Count,
-                    Link = link,
                     Image = image,
                 };
                 
                 features.Add(feature);
-                
-                var editorialLastModified = editorial.Attributes.LastModifiedDate.DateTime;
-                if (editorialLastModified > lastUpdated)
-                    lastUpdated = editorialLastModified;
             }
 
             Feed<Feature> feed = new()
             {
-                Id = grouping.Data.First().Href,
-                Title = "Features",
+                Id = "npr-1108",
+                Title = rssChannel.Element("title")?.Value ?? "Features",
                 Author = new Author
                 {
-                    Name = "Apple Music",
-                    Url = "https://music.apple.com/",
+                    Name = "NPR",
+                    Url = "https://www.npr.org/",
                 },
-                Updated = lastUpdated,
+                Updated = DateTime.TryParse(rssChannel.Element("lastBuildDate")?.Value, out var updated)
+                    ? updated : DateTime.Now,
                 Entries = features,
             };
 
