c3dc194a1f8b39bda5cde707660f9813ae88bb77
Joshua Askharoun <jjask7@gmail.com>
2025-09-19 12:27:47 -0500

    Remove outdated scripts

--- runProd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker compose stop
-docker compose build
-docker compose up -d --remove-orphans
-docker compose logs --follow --tail 0
--- scripts/run_all.ps1
+++ scripts/run_all.ps1
@@ -1,19 +1,23 @@
-param([bool] $Debug = $true, [String[]] $ExcludedServices = @())
+#!/usr/bin/env pwsh
+
+# Runs all microservices in their own PowerShell window
+
+param([bool] $Debug = $true, [bool] $Build = $true, [String[]] $ExcludedServices = @())
 
 $projInfoType = "tuple[String,String]"
 
 $projInfos = @(
-    # New-Object $projInfoType "MetaServices", "1"
+    New-Object $projInfoType "MetaServices", "1"
     New-Object $projInfoType "Catalog", "2"
     New-Object $projInfoType "Commerce", "3"
     New-Object $projInfoType "Catalog.Image", "4"
     New-Object $projInfoType "SocialApi", "5"
-    # New-Object $projInfoType "Comments", "6"
-    # New-Object $projInfoType "Inbox", "7"
+    #New-Object $projInfoType "Comments", "6"
+    New-Object $projInfoType "Inbox", "7"
     New-Object $projInfoType "Mix", "8"
-    # New-Object $projInfoType "Stats", "9"
-    # New-Object $projInfoType "Cache", "10"
-    # New-Object $projInfoType "Tuners", "11"
+    #New-Object $projInfoType "Stats", "9"
+    #New-Object $projInfoType "Cache", "10"
+    New-Object $projInfoType "Tuners", "11"
     New-Object $projInfoType "Tiles", "12"
     New-Object $projInfoType "Login", "14"
 )
@@ -37,7 +41,13 @@ foreach ($projInfo in $projInfos)
         continue
     }
 
-    $command = "dotnet run --no-build --project Zune.Net." + $projName
+    $command = "dotnet run -c " + $configuration + " --project Zune.Net." + $projName
+
+    if ($Build -eq $false)
+    {
+        $command = $command + " --no-build"
+    }
+    
     echo $command
     
     Start-Process powershell.exe -ArgumentList "-noexit", ("-command " + $command)
