e450720c622fea91244d2fc810d3635068691bd2
Yoshi Askharoun <jjask7@gmail.com>
2026-05-12 14:37:27 -0500

    Remove unused incidence matrix code

--- Zune.Net.Shared/Identifiers/PropertyMapperRegistry.cs
+++ Zune.Net.Shared/Identifiers/PropertyMapperRegistry.cs
@@ -7,7 +7,6 @@ namespace Zune.Net.Identifiers;
 public class PropertyMapperRegistry
 {
     private readonly List<IPropertyMapper> _mappers = [];
-    private readonly IncidenceMatrix _incidenceMatrix = new();
 
     public IReadOnlyList<IPropertyMapper> Mappers => _mappers;
 
@@ -23,16 +22,6 @@ public class PropertyMapperRegistry
     public PropertyMapperRegistry RegisterMapper(IPropertyMapper mapper)
     {
         _mappers.Add(mapper);
-
-        foreach (var mapping in mapper.AvailableMappings)
-        {
-            foreach (var prop in mapping.Inputs)
-                AddOrCreateIncidenceRow(prop).InputTo.Add(mapper);
-            
-            foreach (var prop in mapping.Outputs)
-                AddOrCreateIncidenceRow(prop).OutputFrom.Add(mapper);
-        }
-        
         return this;
     }
 
@@ -42,18 +31,6 @@ public class PropertyMapperRegistry
             mr.AvailableMappings
                 .Where(ma => ma.Inputs.IsSubsetOf(inputs))
                 .Select(ma => new PropertyMapperHyperedge(mr, ma)));
-    }
-    
-    public IEnumerable<IPropertyMapper> ForInput(EntityProperty input) => _incidenceMatrix[input].InputTo;
-
-    public IEnumerable<IPropertyMapper> ForOutput(EntityProperty output) => _incidenceMatrix[output].OutputFrom;
-
-    private IncidenceRow AddOrCreateIncidenceRow(EntityProperty property)
-    {
-        if (_incidenceMatrix.TryGetValue(property, out var row))
-            return row;
-
-        return _incidenceMatrix[property] = new IncidenceRow();
     }
 }
 
